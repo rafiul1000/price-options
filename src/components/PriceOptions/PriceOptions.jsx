7729b822fee2764aaf7d960449bf83d0b0aac9cc
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-18 08:53:31 +0600

    Simple Pricing Option using React and Tailwind

--- src/components/PriceOptions/PriceOptions.jsx
+++ src/components/PriceOptions/PriceOptions.jsx
@@ -1,49 +1,53 @@
 import PriceOption from "./PriceOption/PriceOption";
 
 const PriceOptions = () => {
-  {
-    const priceOptions = [
-      {
-        id: 1,
-        name: "Basic Membership",
-        price: "$30/month",
-        features: [
-          "Access to gym equipment",
-          "Locker room access",
-          "Limited group classes",
-        ],
-      },
-      {
-        id: 2,
-        name: "Premium Membership",
-        price: "$60/month",
-        features: [
-          "Unlimited gym access",
-          "Locker room access",
-          "Full access to group classes",
-          "1 personal trainer session/month",
-        ],
-      },
-      {
-        id: 3,
-        name: "Family Membership",
-        price: "$100/month",
-        features: [
-          "Access for 2 adults and 2 children",
-          "Locker room access",
-          "Full access to group classes for all family members",
-          "2 personal trainer sessions/month",
-        ],
-      },
-    ];
-  }
+
+  const priceOptions = [
+    {
+      "id": 1,
+      "name": "Basic Membership",
+      "features": [
+        "Access to gym equipment",
+        "Locker room access",
+        "Limited group classes",
+        "Discounts on gym merchandise"
+      ],
+	"price": 29.99
+    },
+    {
+      "id": 2,
+      "name": "Standard Membership",
+      "features": [
+        "Unlimited gym access",
+        "Locker room access",
+        "Full access to group classes",
+        "Fitness assessment",
+        "Discounted personal trainer sessions"
+      ],
+	"price": 49.99
+    },
+    {
+      "id": 3,
+      "name": "Premium Membership",
+      "features": [
+        "Unlimited gym access",
+        "Locker room access",
+        "Full access to group classes",
+        "Unlimited personal trainer sessions",
+        "Nutrition consultation",
+        "Access to sauna and spa facilities"
+      ],
+	"price": 79.99
+    }
+  ];
+
 
   return (
     <div>
       <h2 className="text-5xl">Best Prices in the Town</h2>
       {
-        priceOptions.map((option => <PriceOption key={option.id} option={option}></PriceOption>
-      ))}
+        priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
+      }
     </div>
   );
 };
